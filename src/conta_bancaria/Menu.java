bbe6b1089d5449fbfbc61da75109bb54470fe92e
LuizEduardoSC <luizeduardoedd1@gmail.com>
2023-11-23 13:26:44 -0300

    Conta_bancaria, Exceptions

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,5 +1,7 @@
 package conta_bancaria;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import conta_bancaria.controller.ContaController;
@@ -10,123 +12,177 @@ import conta_bancaria.util.Cores;
 public class Menu {
 
 	public static void main(String[] args) {
-		
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		ContaController contas = new ContaController();
-		
+
 		int opcao, numero, agencia, tipo, aniversario;
 		String titular;
 		float saldo, limite;
-		
+
 		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000.00f, 100.00f);
 		contas.cadastrar(cc1);
-		
+
 		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 123, 2, "Maria da Silva", 1000.00f, 12);
 		contas.cadastrar(cp1);
-		
+
 		while (true) {
-			
-		System.out.println(Cores.TEXT_BLUE + Cores.ANSI_WHITE_BACKGROUND);
-		
-		System.out.println("*************************************************");		
-		System.out.println("                                                 ");		
-		System.out.println("              BANCO DO BRAZIL COM Z              ");		
-		System.out.println("                                                 ");		
-		System.out.println("*************************************************");		
-		System.out.println("                                                 ");
-		System.out.println("          1 - Criar Conta                        ");
-		System.out.println("          2 - Listar todas as Contas             ");
-		System.out.println("          3 - Buscar conta por Número            ");
-		System.out.println("          4 - Atualizar Dados da Conta           ");
-		System.out.println("          5 - Apagar Conta                       ");
-		System.out.println("          6 - Sacar                              ");
-		System.out.println("          7 - Depositar                          ");
-		System.out.println("          8 - Transferir Valores entre Contas    ");
-		System.out.println("          9 - Sair                               ");
-		System.out.println("                                                 ");		
-		System.out.println("*************************************************");
-		System.out.println("Entre com a opção desejada                       ");
-		System.out.println("                                                 " + Cores.TEXT_RESET);
-		
-		opcao = leia.nextInt();
-		
-		if(opcao == 9) {
-			System.out.println(Cores.TEXT_RESET + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
-			sobre();
-			leia.close();
-			System.exit(0);
-		}
-		
-		switch (opcao) {
-		case 1: System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
-		
-		System.out.println("Digite o número da Agência: ");
-		agencia = leia.nextInt();
-		
-		System.out.println("Digite o nome do Titular: ");
-		leia.skip("\\R");
-		titular = leia.nextLine();
-		
-		System.out.println("Digite o tipo da Conta (1 - CC ou 2 - CP): ");
-		tipo = leia.nextInt();
-		
-		while (tipo < 1 || tipo > 2) {
-			System.out.println("Escolha entre a opção 1 - CC ou 2 - CP)");
-			tipo = leia.nextInt();
-		}
-		
-		System.out.println("Digite o Saldo da Conta: ");
-		saldo = leia.nextFloat();
-		
-		switch(tipo) {
-		    case 1 -> {
-		    	System.out.println("Digite o limite da conta: ");
-		    	limite = leia.nextFloat();
-		    	contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
-		    }
-		    case 2 -> {
-		    	System.out.println("Digite o dia do aniversário da conta: ");
-		    	aniversario = leia.nextInt();
-		    	contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));		    	
-		    }
-		}
-		
-		break;
-		
-		case 2: System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\\n\\n");
-		            contas.listarTodas();
-		break;
-		
-		case 3: System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\\n\\n");
-		break;
-		
-		case 4: System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\\n\\n");
-		break;
-		
-		case 5: System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\\n\\n");
-		break;
-		
-		case 6: System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\\n\\n");
-		break;
-		
-		case 7: System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\\n\\n");
-		break;
-		
-		case 8: System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\\n\\n");
-		break;
-		
-		default:
-			 System.out.println(Cores.TEXT_BLUE +  "\nOpção Inválida! \n" + Cores.ANSI_WHITE_BACKGROUND);
-			 break;
+
+			System.out.println(Cores.TEXT_BLUE + Cores.ANSI_WHITE_BACKGROUND);
+
+			System.out.println("*************************************************");
+			System.out.println("                                                 ");
+			System.out.println("              BANCO DO BRAZIL COM Z              ");
+			System.out.println("                                                 ");
+			System.out.println("*************************************************");
+			System.out.println("                                                 ");
+			System.out.println("          1 - Criar Conta                        ");
+			System.out.println("          2 - Listar todas as Contas             ");
+			System.out.println("          3 - Buscar conta por Número            ");
+			System.out.println("          4 - Atualizar Dados da Conta           ");
+			System.out.println("          5 - Apagar Conta                       ");
+			System.out.println("          6 - Sacar                              ");
+			System.out.println("          7 - Depositar                          ");
+			System.out.println("          8 - Transferir Valores entre Contas    ");
+			System.out.println("          9 - Sair                               ");
+			System.out.println("                                                 ");
+			System.out.println("*************************************************");
+			System.out.println("Entre com a opção desejada                       ");
+			System.out.println("                                                 " + Cores.TEXT_RESET);
+
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Digite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
+
+			if (opcao == 9) {
+				System.out.println(Cores.TEXT_RESET + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+
+				System.out.println("Digite o número da Agência: ");
+				agencia = leia.nextInt();
+
+				System.out.println("Digite o nome do Titular: ");
+				leia.skip("\\R");
+				titular = leia.nextLine();
+
+				System.out.println("Digite o tipo da Conta (1 - CC ou 2 - CP): ");
+				tipo = leia.nextInt();
+
+				while (tipo < 1 || tipo > 2) {
+					System.out.println("Escolha entre a opção 1 - CC ou 2 - CP)");
+					tipo = leia.nextInt();
+				}
+
+				System.out.println("Digite o Saldo da Conta: ");
+				saldo = leia.nextFloat();
+
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite da conta: ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				
+				case 2 -> {
+					System.out.println("Digite o dia do aniversário da conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(
+							new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}
+				}
+
+				keyPress();
+				break;
+
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\\n\\n");
+				contas.listarTodas();
+
+				keyPress();
+				break;
+
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\\n\\n");
+
+				System.out.println("Digite o número da conta: ");
+				numero = leia.nextInt();
+
+				contas.procurarPorNumero(numero);
+
+				keyPress();
+				break;
+
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\\n\\n");
+
+				keyPress();
+				break;
+
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\\n\\n");
+
+				System.out.println(Cores.TEXT_RESET + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				numero = leia.nextInt();
+
+				contas.deletar(numero);
+
+				keyPress();
+				break;
+
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\\n\\n");
+
+				keyPress();
+				break;
+
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\\n\\n");
+
+				keyPress();
+				break;
+
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\\n\\n");
+
+				keyPress();
+				break;
+
+			default:
+				System.out.println(Cores.TEXT_BLUE + "\nOpção Inválida! \n" + Cores.ANSI_WHITE_BACKGROUND);
+				break;
+			}
 		}
 	}
-	}
+
 	public static void sobre() {
 		System.out.println("\n*************************************************");
 		System.out.println("Projeto Desenvolvido por: Luiz Eduardo!");
 		System.out.println("Luiz Eduardo - luizeduardoedd1@gmail.com");
 		System.out.println("github.com/LuizEduardoSC");
 		System.out.println("***************************************************");
+	}
+
+	public static void keyPress() {
+		try {
+
+			System.out.println("\n\nPressione a tecla Enter para continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla inválida!");
+		}
 	}
 }
--- src/conta_bancaria/model/Conta.java
+++ src/conta_bancaria/model/Conta.java
@@ -1,18 +1,21 @@
 package conta_bancaria.model;
 
 public abstract class Conta {
-	
-	// Definir os Atributos da Classe (Quais caracteristicas temos que preencher quando criarmos a conta)
-	// Todo objeto que for criado na classe "Conta" vai obrgatoriamente ter = numero, agencia, tipo, titular e saldo
-	
-	private int numero; // Atributo  numero;
-	private int agencia; // Atributo  agencia;
-	private int tipo; // Atributo  tipo;
-	private String titular; // Atributo  titular;
-	private float saldo; // Atributo  saldo;
-	
+
+	// Definir os Atributos da Classe (Quais caracteristicas temos que preencher
+	// quando criarmos a conta)
+	// Todo objeto que for criado na classe "Conta" vai obrgatoriamente ter =
+	// numero, agencia, tipo, titular e saldo
+
+	private int numero; // Atributo numero;
+	private int agencia; // Atributo agencia;
+	private int tipo; // Atributo tipo;
+	private String titular; // Atributo titular;
+	private float saldo; // Atributo saldo;
+
 	// Criamos o método Construtor
-	// Metodo Construtor = Serve para criar objetos no modelo de dados e vai ter o mesmo nome da classe (sempre começa com Maiúsculo)
+	// Metodo Construtor = Serve para criar objetos no modelo de dados e vai ter o
+	// mesmo nome da classe (sempre começa com Maiúsculo)
 	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
 		this.numero = numero; // Parâmetro (this.numero =)
 		this.agencia = agencia; // Parâmetro this.agencia)
@@ -21,86 +24,76 @@ public abstract class Conta {
 		this.saldo = saldo; // Parâmetro this.saldo =)
 	}
 
-
 	// Criamos os métodos get e set
 	// Métodos de acesso e modificação (get e set)
-	// Sempre vai ser tipado com o mesmo 
+	// Sempre vai ser tipado com o mesmo
 	// Sempre vai ser void pois não vai retornar nada
 	// get e set sempre vai ser minúsculo
 	public int getNumero() {
 		return numero;
 	}
 
-
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-
 
 	public int getAgencia() {
 		return agencia;
 	}
 
-
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
-
 
 	public int getTipo() {
 		return tipo;
 	}
 
-
 	public void setTipo(int tipo) {
 		this.tipo = tipo;
 	}
-
 
 	public String getTitular() {
 		return titular;
 	}
 
-
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
-
 
 	public float getSaldo() {
 		return saldo;
 	}
 
-
 	public void setSaldo(float saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	public boolean sacar(float valor) {
-		
-		if(this.getSaldo() < valor) {
+
+		if (this.getSaldo() < valor) {
 			System.out.println("\nSaldo Insuficiente");
 			return false;
 		}
-		
+
 		this.setSaldo(this.getSaldo() - valor);
 		return true;
 	}
-	
+
 	public void depositar(float valor) {
 		this.setSaldo(this.getSaldo() + valor);
 	}
-	
+
 	// Método auxiliar visualizar
 	public void visualizar() {
-		
+
 		String tipo = "";
-		
+
 		switch (this.tipo) {
 		case 1 -> tipo = "Conta Corrente";
 		case 2 -> tipo = "Conta Poupança";
 		}
-		
+
 		System.out.println("\n\n********************************************");
 		System.out.println("Dados da Conta");
 		System.out.println("********************************************");
@@ -108,6 +101,6 @@ public abstract class Conta {
 		System.out.println("Agência: " + this.agencia);
 		System.out.println("Tipo da Conta: " + tipo);
 		System.out.println("Titular da Conta: " + this.titular);
-		System.out.println("Saldo da Conta: " + this.saldo);		
-	}	
+		System.out.println("Saldo da Conta: " + this.saldo);
+	}
 }
